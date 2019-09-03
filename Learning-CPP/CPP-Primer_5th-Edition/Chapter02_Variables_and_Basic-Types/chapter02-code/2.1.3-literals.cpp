46baadc04612147c4a3512af8f95e99bb0d3bc85
PayneW <2416451355@qq.com>
2019-09-03 23:36:16 +0800

    mac edit 20190903 04

---
+++ Learning-CPP/CPP-Primer_5th-Edition/Chapter02_Variables_and_Basic-Types/chapter02-code/2.1.3-literals.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+// - 2.1.3 —— 字面值常量 (Literals)
+int main() {
+   // -
+   char a = 0;
+   // - In C, `\0` denotes a character with value zero
+   char b = '\0';
+   std::cout << "a:" << a << std::endl;
+   std::cout << "b:" << b << std::endl;
+}
