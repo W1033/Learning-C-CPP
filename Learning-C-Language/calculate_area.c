c54ef2b84becc91f1055a7e42c56c4973a74c200
w1033 <forownwang@gmail.com>
2021-10-21 18:00:45 +0800

    windows edit 2021021 17:57

---
+++ Learning-C-Language/calculate_area.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    /*
+     * 根据海伦公式求三角形的面积
+     *
+     * 三角形三边分别为 a, b, c；三角形的半周长为 p，面积为 S
+     * 
+     * 存在公式定理如下：
+     * p = (a + b + c) / 2
+     * S = sqrt(p(p - a)(p - b)(p - c))
+     */
+    int a, b, c;
+    double p = 0;
+    double S = 0;
+    printf("请输入三角形的三条边: \n");
+    scanf("%f %f %f", &a, &b, &c);
+    p = (a+b+c)/2;
+    S = sqrt(p*(p-a)*(p-b)*(p-c));
+    printf("三角形的面积为: %7.2f\n", S);
+    return 0;
+}
