018bc305209011fe6118522fa3ebe4477a338609
w1033 <forownwang@gmail.com>
2021-10-31 18:37:08 +0800

    Mac edit 20211031 18:56

--- Learning-C-Language/calculate_area.c
+++ Learning-C-Language/calculate_area.c
@@ -11,13 +11,27 @@ int main() {
      * p = (a + b + c) / 2
      * S = sqrt(p(p - a)(p - b)(p - c))
      */
-    int a, b, c;
-    double p = 0;
-    double S = 0;
-    printf("请输入三角形的三条边: \n");
-    scanf("%f %f %f", &a, &b, &c);
-    p = (a+b+c)/2;
-    S = sqrt(p*(p-a)*(p-b)*(p-c));
-    printf("三角形的面积为: %7.2f\n", S);
+    // int a, b, c;
+    // double p = 0;
+    // double S = 0;
+    // printf("请输入三角形的三条边: \n");
+    // scanf("%f %f %f", &a, &b, &c);
+    // p = (a+b+c)/2;
+    // S = sqrt(p*(p-a)*(p-b)*(p-c));
+    // printf("三角形的面积为: %7.2f\n", S);
+    // return 0;
+
+
+    int x, y;
+    printf("请输入 x 的值: ");
+    scanf("%d", &x);
+    if (x < 1) {
+        y = x;
+    }else if (x >= 1 && x < 10) {
+        y = 2 * x - 1;
+    }else if (x >= 10){
+        y = 3 * x - 11;
+    }
+    printf("%d\n", y);
     return 0;
 }
